1af2b4add8ed43a8b56b391e29f6b0437e1b1489
Muhammad Momin <ranamomin@gmail.com>
2021-07-06 13:30:22 -0700

    added todoes in the homepage with the help of map func

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './MyComponents/Header';
+import {Footer} from './MyComponents/Footer';
+import {TodoItem} from './MyComponents/TodoItem';
+import {Todos} from './MyComponents/Todos';
 
 
 function App() {
+  let todos = [
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You need to go to market to get some mangoes."
+    },
+    {
+      sno: 2,
+      title: "Learn React",
+      desc: "Complete the react website you were working on."
+    },
+    {
+      sno: 3,
+      title: "Review Data Structures",
+      desc: "Watch a video on all the data structures for a review."
+    },
+  ]
   return (
     <>
-    <Header/>
+    <Header title="My Todos List" searchBar={false}/>
+    <Todos todos={todos}/>
+    <Footer/>
     </>
   );
 }
