a228df3051f07dfcc957cc59ece18c199a801990
Muhammad Momin <ranamomin@gmail.com>
2022-02-01 01:38:41 -0800

    added lowercase functionality, statatistics, and preview

--- src/MyComponents/TextForm.js
+++ src/MyComponents/TextForm.js
@@ -4,24 +4,46 @@ export default function TextForm(props) {
     const handleUpClick = () => {
         setText(text.toUpperCase());
     };
+    const handleLowerClick = () => {
+      setText(text.toLowerCase());
+    };
      const handleOnChange = (event) => {
        setText(event.target.value);
      };
     const [text, setText] = useState("Enter text here");
   return (
-    <div>
+    <>
+      <div className="container">
         <h1>{props.heading}</h1>
-      <div className="mb--3">
-        <textarea
-          className="form-control"
-          id="myBox"
-          rows="8"
-          value={text}
-          onChange={handleOnChange}
-        ></textarea>
+        <div className="mb--3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary my-3" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary my-3 mx-2" onClick={handleLowerClick}>
+          Convert to Lowercase
+        </button>
       </div>
-      <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to Uppercase</button>
-
-    </div>
+      <div className="container my-2">
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} minutes read</p>
+      </div>
+      <div className='container my-2'>
+          <h1>Preview</h1>
+          <p>
+              {text}
+          </p>
+      </div>
+    </>
   );
 }
